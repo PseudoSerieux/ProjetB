7b44a9e2331c2881de42925b01586860beeb6d30
margaux-dechaud <margaux.dechaud@imie.fr>
2018-12-04 11:44:25 +0100

    Créa d'une class de personnes avec ses instances

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
 <?php
 
-var_dump("coucou");
+class Person { 
+	private $firstname;
+	private $lastname;
+	
+	public function setFirstname($pfirstname) {
+		$this->firstname = $pfirstname;
+	}
+	
+		public function setLastname($plastname) {
+		$this->lastname = $plastname;
+	}
+}
+
+$person1 = new Person();
+// $person1->firstname = "John";
+// $person1->lastname = "Doe";
+$person1->SetFirstname("John");
+$person1->SetLastname("Doe");
+var_dump($person1);
+
+$person2 = new Person();
+// $person2->firstname = "Jane";
+// $person2->lastname = "Die";
+var_dump($person2);
